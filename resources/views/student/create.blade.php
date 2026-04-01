c832ea998c3c13d0169482990a5cbe1b6fbfbdf1
Iren Maharani Ruben <irenmaharani641@gmail.com>
2026-04-02 06:53:52 +0700

    Pertemuan 2: Blade Template

---
+++ resources/views/student/create.blade.php
@@ -0,0 +1,5 @@
+<x-app>
+    <x-slot:title>{{ $title }}</x-slot>
+
+    <h1 class="fw-bold">CREATE student</h1>
+</x-app>
---
+++ resources/views/student/index.blade.php
@@ -0,0 +1,5 @@
+<x-app>
+    <x-slot:title> {{ $title }} </x-slot>
+
+    <h1 class="fw-bold"> data student</h1>
+</x-app>
